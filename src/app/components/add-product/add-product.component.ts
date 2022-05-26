70b14471e1416b29324f4392cce513e942788992
hnativ <lyubomyr.gnativ@gmail.com>
2022-05-26 23:29:51 +0300

    reactive-form: added validators

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from "../../models/product.model";
-import { ProductsService } from "../../services/products.service";
-import {Subscription} from "rxjs";
+import { IProduct } from '../../models/product.model';
+import { ProductsService } from '../../services/products.service';
+import { Subscription, Observable, of } from 'rxjs';
+import { AbstractControl, AsyncValidatorFn, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { IValidatorError } from "../../models/error.model";
 
 @Component({
   selector: 'app-add-product',
@@ -15,12 +17,28 @@ export class AddProductComponent implements OnInit {
 
   product: IProduct = { name: '', price: 0 }
 
+  myInput: FormControl = new FormControl('', [myValidatorLength(2, 5)], []);
+  myForm: FormGroup = new FormGroup({
+    formName: new FormControl('m1'),
+    formPrice: new FormControl('m2'),
+  })
+
   constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
     this.subscription = this.productsService.currentProduct$.subscribe((product: IProduct) => {
       this.isAddProduct = false;
       this.product = product;
+    })
+
+    this.myInput.statusChanges.subscribe(status => {
+      console.log(status);
+      console.log(this.myInput.errors);
+    })
+    this.myForm.valueChanges.subscribe(value => console.log(value));
+    this.myForm.statusChanges.subscribe(status => {
+      console.log(status);
+      console.log(this.myForm.errors);
     })
   }
 
@@ -38,5 +56,27 @@ export class AddProductComponent implements OnInit {
     await this.productsService.updateProduct(product);
     this.isAddProduct = true;
     this.clearFields();
+  }
+}
+
+// validator
+function myValidatorLength(start: number, end: number): ValidatorFn {
+  return function (control: AbstractControl): { [key: string]: IValidatorError } | null
+  {
+    if (control.value.length <= start || control.value.length >= end) {
+      return { myValidatorLength: { message: `From ${start} to ${end}` } }
+    }
+    return null;
+  }
+}
+
+// async validator
+function myValidatorLengthAsync(start: number, end: number): AsyncValidatorFn {
+  return function (control: AbstractControl): Observable<{ [key: string]: IValidatorError } | null>
+  {
+    if (control.value.length <= start || control.value.length >= end) {
+      return of({ myValidatorLength: { message: `From ${start} to ${end}` } })
+    }
+    return of(null);
   }
 }
